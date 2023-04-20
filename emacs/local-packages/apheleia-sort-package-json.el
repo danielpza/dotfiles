b822f5aeb3398ffe758302af1bba9e76b8c73ebd
Daniel Perez Alvarez <danielpza@protonmail.com>
2023-04-20 13:53:09 -0400

    some improvements to sort-package-json apheleia integration

--- emacs/local-packages/apheleia-sort-package-json.el
+++ emacs/local-packages/apheleia-sort-package-json.el
@@ -4,9 +4,12 @@
   (setf (alist-get 'sort-package-json apheleia-formatters)
 	'("sort-package-json" "--stdin")))
 
+(defun apheleia-sort-package-json--is-package-json ()
+  "Return t when the current buffer is visiting a package.json file."
+  (equal (file-name-nondirectory (buffer-file-name)) "package.json"))
+
 (defun apheleia-sort-package-json--set-sort-package-json-formatter ()
-  (message "Hello World")
-  (when (equal (file-name-nondirectory (buffer-file-name)) "package.json")
+  (when (apheleia-sort-package-json--is-package-json)
     (setq-local apheleia-formatter 'sort-package-json)))
 
 (define-minor-mode apheleia-sort-package-json-minor-mode
@@ -15,6 +18,8 @@
   :lighter " SortPkgJSON"
   (if apheleia-sort-package-json-minor-mode
       (add-hook 'json-ts-mode-hook 'apheleia-sort-package-json--set-sort-package-json-formatter 5)
-    (remove-hook 'json-ts-mode-hook 'apheleia-sort-package-json--set-sort-package-json-formatter)))
+    (remove-hook 'json-ts-mode-hook 'apheleia-sort-package-json--set-sort-package-json-formatter))
+  (when (apheleia-sort-package-json--is-package-json)
+    (run-hooks 'json-ts-mode-hook)))
 
 (provide 'apheleia-sort-package-json)
