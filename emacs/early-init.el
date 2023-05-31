2e9b4ad1bf1b29be0e9e5ae9b8fce450e42889d5
Daniel Perez Alvarez <danielpza@protonmail.com>
2023-05-30 23:56:11 -0400

    [emacs] use custom-set-variables early-init

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,11 +1,14 @@
 ;; -*- lexical-binding: t -*-
 (setq comp-deferred-compilation t)
 (setq comp-async-report-warnings-errors nil) ;; silence compilation warnings
-(setq inhibit-startup-screen t) ;; remove startup screen
 
-;; performance
-(setq gc-cons-threshold 100000000) ;; https://emacs-lsp.github.io/lsp-mode/page/performance/#adjust-gc-cons-threshold
-(setq read-process-output-max (* 1024 1024)) ;; https://emacs-lsp.github.io/lsp-mode/page/performance/#increase-the-amount-of-data-which-emacs-reads-from-the-process
+(custom-set-variables
+ '(inhibit-startup-screen t) ;; remove startup screen
+ '(ring-bell-function 'ignore) ;; disable bell
+
+ ;; performance
+ '(gc-cons-threshold 100000000) ;; https://emacs-lsp.github.io/lsp-mode/page/performance/#adjust-gc-cons-threshold
+ '(read-process-output-max (* 1024 1024))) ;; https://emacs-lsp.github.io/lsp-mode/page/performance/#increase-the-amount-of-data-which-emacs-reads-from-the-process
 
 ;; disable ui elements
 (tool-bar-mode 0)
@@ -13,8 +16,6 @@
 (scroll-bar-mode 0)
 
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
-
-(customize-set-variable 'ring-bell-function 'ignore)
 
 ;; visual
 (use-package doom-themes
