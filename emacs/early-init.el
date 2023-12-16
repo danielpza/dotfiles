e568fee402ceaf8ab6c1eebd79efd36ce397a73d
Daniel Perez Alvarez <danielpza@protonmail.com>
2023-12-16 13:40:45 -0500

    format code

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,12 +1,12 @@
 ;; -*- lexical-binding: t -*-
 (setopt comp-deferred-compilation t
-	comp-async-report-warnings-errors nil ;; silence compilation warnings
-	inhibit-startup-screen t ;; remove startup screen
-	ring-bell-function 'ignore ;; disable bell
-	;; performance
-	gc-cons-threshold 100000000 ;; https://emacs-lsp.github.io/lsp-mode/page/performance/#adjust-gc-cons-threshold
-	read-process-output-max (* 1024 1024) ;; https://emacs-lsp.github.io/lsp-mode/page/performance/#increase-the-amount-of-data-which-emacs-reads-from-the-process
-	)
+		comp-async-report-warnings-errors nil ;; silence compilation warnings
+		inhibit-startup-screen t ;; remove startup screen
+		ring-bell-function 'ignore ;; disable bell
+		;; performance
+		gc-cons-threshold 100000000 ;; https://emacs-lsp.github.io/lsp-mode/page/performance/#adjust-gc-cons-threshold
+		read-process-output-max (* 1024 1024) ;; https://emacs-lsp.github.io/lsp-mode/page/performance/#increase-the-amount-of-data-which-emacs-reads-from-the-process
+		)
 
 ;; disable ui elements
 (tool-bar-mode 0)
