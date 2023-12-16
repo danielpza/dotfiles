93c9aed642f39b124a34d64915c712852ab52a82
Daniel Perez Alvarez <danielpza@protonmail.com>
2023-12-16 15:09:33 -0500

    WIP c1 config

--- configuration-common.nix
+++ configuration-common.nix
@@ -4,6 +4,10 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
   # Enable the GNOME Desktop Environment. https://nixos.wiki/wiki/GNOME
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
@@ -57,7 +61,10 @@
 
   # Enable sound with pipewire.
   sound.enable = true;
+  hardware.bluetooth.enable = true; # TODO check if needed
   hardware.pulseaudio.enable = false;
+  hardware.pulseaudio.package = pkgs.pulseaudioFull;
+  hardware.enableAllFirmware = true; # TODO check if needed
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -66,6 +73,11 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     jack.enable = true;
-    # package = pkgs.pipewire_0_2;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
