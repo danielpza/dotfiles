007d4ea386c0bdecff4887e6d4a8ca34d110790e
Daniel Perez Alvarez <danielpza@protonmail.com>
2023-12-16 13:33:00 -0500

    update config

--- c2-configuration.nix
+++ c2-configuration.nix
@@ -19,24 +19,6 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    jack.enable = true;
-    # package = pkgs.pipewire_0_2;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
@@ -44,15 +26,6 @@
   # $ nix search wget
   # environment.systemPackages = with pkgs; [ logitech-udev-rules solaar ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
--- configuration-common.nix
+++ configuration-common.nix
@@ -54,4 +54,18 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    jack.enable = true;
+    # package = pkgs.pipewire_0_2;
+  };
 }
