f85ee56cd5bc0fa2c3e2cd3a34a972ac5a28f300
Daniel Perez Alvarez <danielpza@protonmail.com>
2023-12-27 12:08:57 -0500

    gnome extension caffeine

--- home-gnome.nix
+++ home-gnome.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
 
-  home.packages = with pkgs; [ gnome-feeds ];
+  home.packages = with pkgs;
+    [ gnome-feeds ] ++ (with pkgs.gnomeExtensions; [ caffeine ]);
 
   # GNOME configuration
   dconf.settings = {
